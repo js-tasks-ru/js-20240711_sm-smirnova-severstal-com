2d26c95ee24068e6aa3e0eccfcb4c60acf448026
Смирнова Светлана Михайловна <sm.smirnova@severstal.com>
2024-07-20 22:33:34 +0300

    getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,18 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+     return function (obj) {
+        const arr = path.split(".");
+        const iterator1 = arr[Symbol.iterator]();
+        let value = obj;
+        for (const prop of iterator1) {
+            if (Object.hasOwn(value, prop)){
+                value = value[prop];
+            } else {
+                return;
+            }
+        }
+        return value;
+    }
+  
 }
