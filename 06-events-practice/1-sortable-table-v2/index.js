ac7d231c66e91d4580893d9dbb7a293549889c6c
Смирнова Светлана Михайловна <sm.smirnova@severstal.com>
2024-08-02 21:02:52 +0300

    06 events

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,59 @@
-export default class SortableTable {
+import SortableTable1 from './sortableTable.js';
+
+export default class SortableTable  {
+  subElements = {};
+
   constructor(headersConfig, {
     data = [],
     sorted = {}
   } = {}) {
+    this.headersConfig = headersConfig;
+    this.data = data;
+    this.sorted = sorted;
+    this._sortableTable = new SortableTable1(headersConfig, data);
+    this.element = this._sortableTable.element;
+    this.selectSubElements();
+    this.sort();
+    this.addClick();
+  }
+
+  selectSubElements() {
+    this.element.querySelectorAll("[data-element]").forEach((element) => {
+      this.subElements[element.dataset.element] = element;
+    });
+  }
+
+  addClick(){
+    this.subElements.header.addEventListener('click', function(event) {
+      const target = event.target; // где был клик?
+    
+      if (target.tagName != 'SPAN') return; 
+      const order = this.sorted.map((item) => {
+        const { id, order } = item;        
+        return order;
+      });
+      this.sort(target.field, order); 
+    });
 
   }
+
+  sort(field, order) {
+    if (this.isSortLocally) {
+      this.SortableTable1.sort(field, order);
+    } else {
+      this.sortOnServer();
+    }
+  }
+
+  sortOnServer(){
+
+  }
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
+
 }
--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,7 +1,60 @@
 class Tooltip {
-  initialize () {
+  static instance;
 
+  constructor () { 
+    if (Tooltip.instance) {
+      return Tooltip.instance; 
+    }
+    Tooltip.instance = this;   
   }
+  
+  initialize () {
+    document.addEventListener('pointerover', function(event) {
+   
+    const target = event.target;
+
+     // если у нас есть подсказка...
+    const id = target.dataset.tooltip;
+    if (!id) return;
+
+     // ...создадим элемент для подсказки
+     this.tooltipElement = document.createElement('div');
+     this.tooltipElement.className = 'tooltip';
+     this.tooltipElement.innerHTML = id;
+     document.body.append(this.tooltipElement);
+
+     const div = target.closest('div')
+     if (!div) return; 
+
+     if (!document.contains(div)) return; 
+   });
+
+   document.addEventListener('pointerout', function(event) {
+
+     if (this.tooltipElement) {
+      this.tooltipElement.remove();
+      this.tooltipElement = null;
+     }
+
+   });
+   document.addEventListener('pointermove', (event) => {
+      const target = event.target;
+
+});
+
+}
+
+  render(){
+    
+  }
+
+  destroy() {
+  if (this.tooltipElement) {
+      this.tooltipElement.remove();
+      this.tooltipElement = null;
+    }
+  }
+
 }
 
 export default Tooltip;
